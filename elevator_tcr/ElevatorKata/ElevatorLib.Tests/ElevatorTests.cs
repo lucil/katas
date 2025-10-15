d4edbb76f7b7ca9e27220762342bf351c89f5185
Trebicka, Emil <Emil.Trebicka@xtech.ai>
2025-10-15 10:54:27 +0200

    Add ProjectReference to ElevatorLib in test project

--- elevator_tcr/ElevatorKata/ElevatorLib.Tests/ElevatorTests.cs
+++ elevator_tcr/ElevatorKata/ElevatorLib.Tests/ElevatorTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO.Compression;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,8 +12,71 @@ namespace ElevatorLib.Tests
         [Fact]
         public void StartsAtFloorZero()
         {
-            var elevator = new Elevator();
+            var elevator = ElevatorTestsExtensions.CreateTestElevator();
             Assert.Equal(0, elevator.GetCurrentFloor());
         }
+
+        [Fact(DisplayName ="Given the elevator is at floor 0, when a request is made to move to floor 5, then the elevator moves to floor 5")]
+        public void MovesToFloorFiveFromFloorZero()
+        {
+            var elevator = ElevatorTestsExtensions.CreateTestElevator();
+            elevator.RequestFloor(5);
+            Assert.Equal(5, elevator.GetCurrentFloor());
+        }
+
+        [Fact(DisplayName = "Given the elevator is at floor 3, when a request is made to move to floor 10, then the elevator ignores the request.")]
+        public void WhenElevetorIsToFloorThreeIgnoresRequestFromFloorTen()
+        {
+            var elevator = ElevatorTestsExtensions.CreateTestElevator(startingFloor: 3);
+
+            elevator.RequestFloor(10);
+
+            Assert.Equal(3, elevator.GetCurrentFloor());
+        }
+
+        [Fact(DisplayName = "Given the elevator is at floor 3, when a request is made to move to floor -1, then the elevator ignores the request.")]
+        public void WhenElevetorIsToFloorThreeIgnoresRequestFromFloorMinus1()
+        {
+            var elevator = ElevatorTestsExtensions.CreateTestElevator(startingFloor: 3);
+
+            elevator.RequestFloor(-1);
+
+            Assert.Equal(3, elevator.GetCurrentFloor());
+        }
+        
+        [Fact(DisplayName ="Given the elevator is at floor 0, when requests are made for floors 4 and 2 in that order, then the elevator moves to floor 4 first and then to floor 2.")]
+        public void RequestsAreProcessedInAFIFOOrder()
+        {
+            var elevator = ElevatorTestsExtensions.CreateTestElevator(startingFloor: 0);
+
+            elevator.RequestFloor([4, 2]);
+
+            var movementLogs = elevator.GetOpenedDoors();
+
+            Assert.Equal(new List<int> { 0, 4, 2 }, movementLogs);
+        }
+        
+        [Fact(DisplayName ="Given the elevator is at floor 3, when requests are made for floors 5 (up), 7 (up), and 2 (down), then the elevator moves to floor 5, then floor 7, and only after completing all \"up\" requests, it moves to floor 2.")]
+        public void RequestsAreProcessedInAFIFOORderFollowingTheDirection()
+        {
+            var elevator = ElevatorTestsExtensions.CreateTestElevator(startingFloor: 3);
+
+            elevator.RequestFloor([5, 7, 2]);
+
+            var movementLogs = elevator.GetOpenedDoors();
+
+            Assert.Equal(new List<int> {3, 5, 7, 2 }, movementLogs);
+        }
     }
+
+    public static class ElevatorTestsExtensions
+    {
+        public static Elevator CreateTestElevator(int minFloor = 0, int maxFloors = 10, int startingFloor = 0)
+        {
+            var elevator = new Elevator(minFloor, maxFloors);
+            elevator.RequestFloor(startingFloor);
+            return elevator;
+        }
+    }
+    
 }
--- elevator_tcr/ElevatorKata/ElevatorLib/Elevator.cs
+++ elevator_tcr/ElevatorKata/ElevatorLib/Elevator.cs
@@ -2,16 +2,43 @@
 {
     public class Elevator
     {
+        private int _minFloor;
+        private int _maxFloors;
         private int _currentFloor;
+        private List<int> _movementLogs = new List<int>();
 
-        public Elevator()
+        public Elevator(int minFloor, int maxFloors)
         {
-            _currentFloor = 0; // Elevator starts at ground floor
+            _minFloor = minFloor;
+            _maxFloors = maxFloors;
+            _currentFloor = minFloor;
         }
 
         public int GetCurrentFloor()
         {
             return _currentFloor;
+        }
+
+        public List<int> GetOpenedDoors()
+        {
+            return _movementLogs;
+        }
+
+        public void RequestFloor(params int[] floors)
+        {
+            foreach (var floor in floors)
+            {
+                if (floor >= _minFloor && floor < _maxFloors)
+                {
+                    SetCurrentFloor(floor);
+                }
+            }
+        }
+        
+        private void SetCurrentFloor(int floor)
+        {
+            _currentFloor = floor;
+            _movementLogs.Add(floor);        
         }
     }
 }
